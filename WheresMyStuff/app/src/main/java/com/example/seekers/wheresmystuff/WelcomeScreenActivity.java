6be394da457f65984d1dac83d601e3620b2a96be
pritshah98 <addison20>
2017-06-27 18:05:01 -0400

    Fixed database bug

--- WheresMyStuff/app/src/main/java/com/example/seekers/wheresmystuff/WelcomeScreenActivity.java
+++ WheresMyStuff/app/src/main/java/com/example/seekers/wheresmystuff/WelcomeScreenActivity.java
@@ -41,6 +41,49 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
 
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DataSnapshot admins = dataSnapshot.child("Admins");
+                Iterable<DataSnapshot> adminsChildren = admins.getChildren();
+                for (DataSnapshot admin: adminsChildren) {
+                    Admin a = admin.getValue(Admin.class);
+                    if (a != null) {
+                        personList.getPersonList().put(a.getPassword(), a);
+                    }
+                }
+                DataSnapshot users = dataSnapshot.child("Users");
+                Iterable<DataSnapshot> usersChildren = users.getChildren();
+                for (DataSnapshot user: usersChildren) {
+                    User u = user.getValue(User.class);
+                    if (u != null) {
+                        personList.getPersonList().put(u.getPassword(), u);
+                    }
+                }
+                DataSnapshot lostItems = dataSnapshot.child("LostItems");
+                Iterable<DataSnapshot> lostChildren = lostItems.getChildren();
+                for (DataSnapshot lost: lostChildren) {
+                    LostItem l = lost.getValue(LostItem.class);
+                    if (l != null) {
+                        lostItemList.getLostItemList().add(l);
+                    }
+                }
+                DataSnapshot foundItems = dataSnapshot.child("FoundItems");
+                Iterable<DataSnapshot> foundChildren = foundItems.getChildren();
+                for (DataSnapshot found: foundChildren) {
+                    FoundItem f = found.getValue(FoundItem.class);
+                    if (f != null) {
+                        foundItemList.getFoundItemList().add(f);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         registration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,48 +96,6 @@ public class WelcomeScreenActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(WelcomeScreenActivity.this, LoginScreenActivity.class);
                 startActivity(intent);
-                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        DataSnapshot admins = dataSnapshot.child("Admins");
-                        Iterable<DataSnapshot> adminsChildren = admins.getChildren();
-                        for (DataSnapshot admin: adminsChildren) {
-                            Admin a = admin.getValue(Admin.class);
-                            if (a != null) {
-                                personList.getPersonList().put(a.getPassword(), a);
-                            }
-                        }
-                        DataSnapshot users = dataSnapshot.child("Users");
-                        Iterable<DataSnapshot> usersChildren = users.getChildren();
-                        for (DataSnapshot user: usersChildren) {
-                            User u = user.getValue(User.class);
-                            if (u != null) {
-                                personList.getPersonList().put(u.getPassword(), u);
-                            }
-                        }
-                        DataSnapshot lostItems = dataSnapshot.child("LostItems");
-                        Iterable<DataSnapshot> lostChildren = lostItems.getChildren();
-                        for (DataSnapshot lost: lostChildren) {
-                            LostItem l = lost.getValue(LostItem.class);
-                            if (l != null) {
-                                lostItemList.getLostItemList().add(l);
-                            }
-                        }
-                        DataSnapshot foundItems = dataSnapshot.child("FoundItems");
-                        Iterable<DataSnapshot> foundChildren = foundItems.getChildren();
-                        for (DataSnapshot found: foundChildren) {
-                            FoundItem f = found.getValue(FoundItem.class);
-                            if (f != null) {
-                                foundItemList.getFoundItemList().add(f);
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
             }
         });
     }
